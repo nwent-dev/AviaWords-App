8fe99145f609876540bc7671bd2ac5ef3d8e6d7c
nwent-dev <nwent@proton.me>
2025-03-05 19:36:57 +0300

    Initial Commit

---
+++ AviaWords/AviaWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviaWordsApp.swift
+//  AviaWords
+//
+//  Created by Александр Калашников on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AviaWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
